0b7c0c5f47e0e0fdee8831d8d42c76f95055ad9c
Alex Armeni <Alex.armeni12@gmail.com>
2026-05-25 09:25:32 -0300

    e) agregar TrianguloIsosceles y TrianguloEquilatero

--- Figura.cs
+++ Figura.cs
@@ -62,4 +62,50 @@ namespace Figuras
             graphics.DrawEllipse(pen,x,y, radio, radio);
         }
     }
+public class TrianguloIsosceles : Figura
+{
+    protected int baseAncho;
+    protected int altura;
+
+    public TrianguloIsosceles(int baseAncho, int altura)
+    {
+        this.baseAncho = baseAncho;
+        this.altura = altura;
+    }
+
+    public override void Dibujar(Pen pen, Graphics graphics, int x, int y)
+    {
+        Point[] puntos = new Point[3]
+        {
+            new Point(x + baseAncho / 2, y),              // vértice superior (centro)
+            new Point(x,                 y + altura),      // base izquierda
+            new Point(x + baseAncho,     y + altura)       // base derecha
+        };
+        graphics.DrawPolygon(pen, puntos);
+    }
+}
+
+public class TrianguloEquilatero : Figura
+{
+    protected int lado;
+
+    public TrianguloEquilatero(int lado)
+    {
+        this.lado = lado;
+    }
+
+    public override void Dibujar(Pen pen, Graphics graphics, int x, int y)
+    {
+        // La altura de un triángulo equilátero es lado * √3/2
+        int altura = (int)(lado * Math.Sqrt(3) / 2);
+
+        Point[] puntos = new Point[3]
+        {
+            new Point(x + lado / 2, y),          // vértice superior
+            new Point(x,            y + altura),  // base izquierda
+            new Point(x + lado,     y + altura)   // base derecha
+        };
+        graphics.DrawPolygon(pen, puntos);
+    }
+}
 }
